7e02530c2cab1457c246158bd94470e9a3b26a6c
erixjavier <mendozaeria@globalhitss.com>
2019-07-28 10:07:07 -0500

    reajuste y login funcionando correctamente

--- app/src/main/java/com/example/authloginfirebases/MainActivity.kt
+++ app/src/main/java/com/example/authloginfirebases/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.authloginfirebases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.authloginfirebases.LoginActivity.Companion.USERNAME_KEY
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var username: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        username = intent.extras!![USERNAME_KEY]!!.toString()
+        usernameTextView.text = username
     }
 }
